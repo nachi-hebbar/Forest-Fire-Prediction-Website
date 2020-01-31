39c656ab6b85c82b47d36701f0975dd26fb924ec
Nachiketa Hebbar <54833985+nachi-hebbar@users.noreply.github.com>
2020-01-31 15:12:42 +0530

    Add files via upload

---
+++ forest_fire.py
@@ -0,0 +1,34 @@
+#!C:\Users\Lenovo\AppData\Local\Programs\Python\Python37-32\python.exe
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import warnings
+import pickle
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv("Forest_fire.csv")
+data = np.array(data)
+
+X = data[1:, 1:-1]
+y = data[1:, -1]
+y = y.astype('int')
+X = X.astype('int')
+# print(X,y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+log_reg = LogisticRegression()
+
+
+log_reg.fit(X_train, y_train)
+
+inputt=[int(x) for x in "45 32 60".split(' ')]
+final=[np.array(inputt)]
+
+b = log_reg.predict_proba(final)
+
+
+pickle.dump(log_reg,open('model.pkl','wb'))
+model=pickle.load(open('model.pkl','rb'))
+
+
